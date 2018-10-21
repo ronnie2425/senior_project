81019f75162bd8e01aa6c3111777ee87b86389b3
ronnie2425 <ronniestevenson2425@gmail.com>
2018-10-21 13:52:29 -0700

    test

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -33,8 +33,8 @@ public class LoginServlet extends HttpServlet {
         String errorMessage = null;
         Double result = null;
         try {
-          String user = getStringFromParameter(req.getParameter("Username"));
-          String pass = getStringFromParameter(req.getParameter("Password"));
+          String user = req.getParameter("Username");
+          String pass = req.getParameter("Password");
 
 
           if (user == null || pass == null) {
