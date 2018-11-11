b7e0f86bd4073e1c36cfa9eab9c6ca32d5321c77
ronnie2425 <ronniestevenson2425@gmail.com>
2018-11-10 16:28:58 -0800

    fixed servlet and jsp connectivity.........

--- src/main/java/servlets/SignUpServlet.java
+++ src/main/java/servlets/SignUpServlet.java
@@ -7,6 +7,7 @@ import java.util.List;
 import controllers.LoginController;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /*Quarantine for errors*/
 //TODO update with business variables
-
+@WebServlet("/signUpServlet")
 public class SignUpServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
