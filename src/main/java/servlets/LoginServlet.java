8870379f9f2a47d3b8c6d268d92248460d7f172c
ronnie2425 <ronniestevenson2425@gmail.com>
2018-11-10 16:31:39 -0800

    named servlets

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import controllers.LoginController;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 /*Quarantine for errors*/
-
+@WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
