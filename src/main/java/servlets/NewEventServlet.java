0ff68711d895fe8fec6a3c3817a1148ca125eddf
David <dhenry5@ycp.edu>
2018-11-18 12:21:18 -0500

    changes to string to int in new event servlet

--- src/main/java/servlets/NewEventServlet.java
+++ src/main/java/servlets/NewEventServlet.java
@@ -53,13 +53,16 @@ public class NewEventServlet extends HttpServlet {
           //req.getParameter("Business");	//May not work
           String location = req.getParameter("Location");
           errorMessage = "falure between time and parameters";
-          String array[]=time1.split("/");
-          int time= (Integer.parseInt(array[0])-2000) + (Integer.parseInt(array[2])*100) +(Integer.parseInt(array[0])*10000);
-          String array1[]=start1.split(":");
-          int start= (Integer.parseInt(array1[2])) + (Integer.parseInt(array1[1])*100) +(Integer.parseInt(array1[0])*10000);
-          String array2[]=end1.split(":");
-          int end= (Integer.parseInt(array2[2])) + (Integer.parseInt(array2[1])*100) +(Integer.parseInt(array2[0])*10000);
-         
+
+//        String array[]=time1.split("-");
+//        int time= (Integer.parseInt(array[0])-2000) + (Integer.parseInt(array[2])*100) +(Integer.parseInt(array[0])*10000);
+//        String array1[]=start1.split(":");
+//        int start= (Integer.parseInt(array1[2])) + (Integer.parseInt(array1[1])*100) +(Integer.parseInt(array1[0])*10000);
+//        String array2[]=end1.split(":");
+//        int end= (Integer.parseInt(array2[2])) + (Integer.parseInt(array2[1])*100) +(Integer.parseInt(array2[0])*10000);
+        	int start = Integer.parseInt(start1);
+        	int end =  Integer.parseInt(end1);
+        	int time =  Integer.parseInt(time1);
           errorMessage = "failed at first if";
           
           
