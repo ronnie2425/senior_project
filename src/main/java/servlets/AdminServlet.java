1f7df5bef32976e9347a68bcce08257f18c6b96a
Ron Stevenson <ronniestevenson2425@gmail.com>
2018-11-18 20:28:07 -0500

    created admin page and servlet, testing it

---
+++ src/main/java/servlets/AdminServlet.java
@@ -0,0 +1,97 @@
+package servlets;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controllers.*;
+import database.Databasequeries;
+import model.*;
+
+
+/*Quarantine for errors*/
+@WebServlet("/adminServlet")
+public class AdminServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String username=null;
+		Cookie[] cks=req.getCookies();
+		if (cks !=null){
+			for (int i=0;i<cks.length;i++){
+				String name=cks[i].getName();
+				username = cks[i].getValue();
+				if (name.equals("auth")){
+					break;
+				}
+			}
+		}
+		//if the user is not logged in send to login page
+		if (username == null){
+			req.getRequestDispatcher("login.jsp").forward(req, resp);
+		}
+		//if the user is logged in send to user info page
+		else{
+			//grab all of the events of the user's business
+		
+			Databasequeries queries = new Databasequeries();
+			
+			
+			try {
+				User user=queries.findAccountByName(username).get(0);
+				List<Business> b=queries.findBusinesssFromAccount(username);
+			
+			EventController controller = new EventController();
+			List<Event> list=new ArrayList<Event>();
+			
+			for (int i=0;i<b.size();i++){
+			list.addAll(controller.findEventByBusiness(b.get(i).getName()));
+			}
+			req.setAttribute("eventlist", list);
+			
+			
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			req.getRequestDispatcher("admin.jsp").forward(req, resp);
+
+		}
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		String time = req.getParameter("time");
+		String start = req.getParameter("start");
+		String location = req.getParameter("location");
+		String description = req.getParameter("description");
+		
+		if(!req.getParameter("eventId").equals(null)){
+			//redirect to edit the selected post
+			int postid = Integer.parseInt(req.getParameter("postID"));
+			req.getRequestDispatcher("editEvent.jsp").forward(req, resp);
+			
+		
+			
+		}
+		
+		else{
+			req.setAttribute("eventselected", name);
+		}
+		req.getRequestDispatcher("userInfo.jsp").forward(req, resp);
+
+	}
+	
+}
+
---
+++ src/main/webapp/admin.jsp
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<html>
+
+	<head>
+		<title>User Info</title>
+		<link href="indexstyle.css" rel="stylesheet" type="text/css" ></link>
+	</head>
+	
+	<body>
+		<div id="background">
+			<form action="adminServlet" method="post">
+			<div>
+				<c>
+					<li>Username is: ${user.username}</li><br />
+					<li>Password is: ${user.password}</li><br />
+				</c>
+				<c:forEach items="${list}" var="event">
+			        	Event is: ${event.name}
+			        	Starts at: ${event.time}
+			        	On: ${event.start}
+			        	At: ${event.location}
+			        	Description: ${event.description}         
+			        	<form action="${pageContext.servletContext.contextPath}/event.jsp" method="get">
+							<br>Click here to view info about the event</br>
+							<input name="name" type="text" value="${event.name}" hidden />
+							<input name="time" type="text" value="${event.time}" hidden />
+							<input name="start" type="text" value="${post.start}" hidden />
+							<input name="location" type="text" value="${post.location}" hidden />
+							<input name="description" type="text" value="${post.description}" hidden />
+							
+							
+							<input name="post" type="submit" value="View"/>
+						</form>
+						<form action="${pageContext.servletContext.contextPath}/editEvent" method="post">
+							<br>Click here to edit the event</br>
+							<input name="eventId" type="text" value="${event.Id}" hidden />
+							<input name="post" type="submit" value="Edit"/>
+						</form>		
+						<br />	            
+				</c:forEach>
+			</div>
+			<input name ="submit" type ="submit" value="Search"/>
+			</form>
+		</div>
+	</body>
+	
+</html>
