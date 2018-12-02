1d2e81c055a5d4296490f1838c17095abbc875ad
Matt Ritchie <mritchie1@ycp.edu>
2018-12-02 16:33:28 -0500

    Testing false error on login

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -35,7 +35,7 @@ public class LoginServlet extends HttpServlet {
 		// Decode form parameters and dispatch to controller
         String errorMessage = null;
         
-          //try {
+          try {
           String user = getStringFromParameter(req.getParameter("Username"));
           String pass = getStringFromParameter(req.getParameter("Password"));
 
@@ -46,8 +46,8 @@ public class LoginServlet extends HttpServlet {
           else { //fields filled
             LoginController controller = new LoginController();
             
-           // if(controller.verifyAccount(user, controller.gimmeSalt(pass))){
-            if (true) {														//TODO Remove. Terrible code practice.
+          if(controller.verifyAccount(user, controller.gimmeSalt(pass))){
+            //if (true) {														//TODO Remove. Terrible code practice.
             	//resp.sendRedirect(req.getContextPath() + "/businessList.jsp");
             	//req.getSession().setAttribute("username", user);	
             	Cookie ck=new Cookie("auth",user);
@@ -62,10 +62,10 @@ public class LoginServlet extends HttpServlet {
             
           }//end fields filled else
           
-       // } catch(Exception e) {
-       //   errorMessage = "There was a problem in the LoginServlet. check verifyaccount's isEmpty return val.";
-       //   req.setAttribute("errorMessage", errorMessage);
-       // }
+        } catch(Exception e) {
+          errorMessage = "There was a problem in the LoginServlet. check verifyaccount's isEmpty return val.";
+          req.setAttribute("errorMessage", errorMessage);
+        }
         
         //display
         req.getRequestDispatcher("login.jsp").forward(req, resp);
