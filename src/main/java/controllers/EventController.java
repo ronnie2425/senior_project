42777bc5fc0822c5aa4375045533fc68f4eace36
David <dhenry5@ycp.edu>
2018-12-02 15:23:59 -0500

    removing rons comments from insert event in the controller

--- src/main/java/controllers/EventController.java
+++ src/main/java/controllers/EventController.java
@@ -101,12 +101,12 @@ public class EventController {
 			events = queries.findEventByName(name);
 	
 
-			//if (events.isEmpty()) {
+			if (events.isEmpty()) {
 				
-				//queries.insertEvent(name, description, start_date, end_date, time, business, location,id);
+				queries.insertEvent(name, description, start_date, end_date, time, business, location,id);
 			return true;
-			//}
-				
+			}
+			return false;
 
 //			
 //			
